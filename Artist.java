54a852fc03499f0082910e0085f92af15858a3e1
Rana Umair <ranaumairjabbar09@gmail.com>
2024-09-19 15:41:22 +0500

    Add files via upload

---
+++ Artist.java
@@ -0,0 +1,18 @@
+public class Artist{
+	private String name;
+	public Artist(Artist artist){
+		this.name=artist.getname;}
+
+	public String getname(){
+			return name;}
+	public void display(){
+	System.out.println("ARTIST NAME:"+getname());
+
+			}
+	
+
+
+
+
+
+}
---
+++ Artwork.java
@@ -0,0 +1,16 @@
+public class Artwork{
+	private String title;
+	private int year;
+	private String name;
+	public void settitle(String t){
+	 	title=t;}
+	public String gettitle(){
+	 return	title;}
+	public void setyear(int y){
+	 	y=year;}
+	public int getyear(){
+	 return	year;}
+	
+
+
+}
